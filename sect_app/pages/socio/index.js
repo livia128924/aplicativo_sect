8b1c5571bb4bd9d2b1930e6ff3960ceb3310cf92
livia <livia128924@gmail.com>
2021-05-25 17:33:44 -0300

    adicionando mais steps do formulario

--- sect_app/pages/socio/index.js
+++ sect_app/pages/socio/index.js
@@ -10,24 +10,76 @@ function ExampleOne ()  {
   //const onSubmit = data => console.log(data);
 
   
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
-  const [items, setItems] = useState([
+  const [aberto, setAberto] = useState(false);
+  const [valor, setValor] = useState(null);
+  const [item, setItem] = useState([
     {label: 'Apple', value: 'apple'},
     {label: 'Banana', value: 'banana'}
   ]);
 
-useEffect (()=>{
+    
+  const [abertoAcesso, setAbertoAcesso] = useState(false);
+  const [valorAcesso, setValorAcesso] = useState(null);
+  const [itemAcesso, setItemAcesso] = useState([
+    {label: 'samsung', value: 'samsung'},
+    {label: 'motorola', value: 'motorola'}
+  ]);
+  
+  const [abertoAbrangencia, setAbertoAbrangencia] = useState(false);
+  const [valorAbrangencia, setValorAbrangencia] = useState(null);
+  const [itemAbrangencia, setItemAbrangencia] = useState([
+    {label: '100', value: '100'},
+    {label: '200', value: '200'}
+  ]);
+
+  const [openMao_de_obra, setOpenMao_de_obra] = useState(false);
+  const [valorMao_de_obra, setValorMao_de_obra] = useState(null);
+  const [itemMao_de_obra, setItemMao_de_obra] = useState([
+    {label: '500', value: '500'},
+    {label: '600', value: '600'}
+  ]);
+  
+  const [openSe_ruj_associados, setOpenSe_ruj_associados] = useState(false);
+  const [valorSe_ruj_associados, setValorSe_ruj_associados] = useState(null);
+  const [itemSe_ruj_associados, setItemSe_ruj_associados] = useState([
+    {label: '50', value: '50'},
+    {label: '60', value: '60'}
+  ]);
+
+  const [openSe_ruj_cooperados, setOpenSe_ruj_cooperados] = useState(false);
+  const [valorSe_ruj_cooperados, setValorSe_ruj_cooperados] = useState(null);
+  const [itemSe_ruj_cooperados, setItemSe_ruj_cooperados] = useState([
+    {label: '70', value: '70'},
+    {label: '80', value: '80'}
+  ]);
+
+
+  useEffect (()=>{
   
       api.post('socio/municipios.php', {})
       .then(function (response) {
          
           const {label}= response.data;
           console.log(response.data);
-          setItems(response.data);
+          setItem(response.data);
       })
 },[]);
 
+const [openDescricao, setOpenDescricao] = useState(false);
+const [valorAtividade, setValorAtividade ] = useState(null);
+
+
+const [itemDescricao, setItemDescricao] = useState([
+  {label: 'descricao', value: 'descricao'},
+  {label: 'valor', value: 'valor'}
+]);
+
+const [openNaturezaAtv, setOpenNaturezaAtv] = useState(false);
+const [valorNaturezaAtv, setValorNaturezaAtv] = useState(null);
+const [itemNaturezaAtv, setItemNaturezaAtv] = useState([
+  {label: 'val', value: 'val'},
+  {label: 'lab', value: 'lab'}
+]);
 
 
 async function requerente (dados) {
@@ -38,7 +90,7 @@ async function requerente (dados) {
    .then(function (response) {
 
     const {nome}=response.data;
-    console.log(response.data);
+   
     setNome(response.data);
    })
    .catch(function (error) {
@@ -48,7 +100,12 @@ async function requerente (dados) {
 
   
   const [localizacao, setLocalizacao] = useState('');
-  const [text, onChangeText] = useState('');
+  const [outros, setOutros] = useState('');
+  const [ comercio, setComercio] = useState('');
+  const [ industria, setIndustria] = useState('');
+  const [ recursosNaturais, setRecursosNaturais] = useState('');
+  const [ t_I, setT_I] = useState('');
+
   const [number,onChangeNumber] = useState('');
 
 
@@ -89,42 +146,43 @@ async function requerente (dados) {
             label="Area"
             onNext={onformularioStepComplete}
             onPrevious={onPrevStep}
-            scrollable={true}
-
           >
-            <View style={styles.form}>
-            <View style={styles.rect2}>
-              <Text style={styles.titulo} >ÁREA DE ABRANGÊNCIA</Text>
+        <View style={styles.form}>
+          <View style={styles.rect2}>
+              <Text>ÁREA DE ABRANGÊNCIA</Text>
+          </View>
+            <View style={styles.municipio}>
+              <Text >MUNICIPIOS</Text>
+              </View>
             <View>
-            <Text style={styles.municipio}>MUNICIPIOS</Text>
-            </View>
-            <View style={{ alignItems: 'center' }}>
             <DropDownPicker
             style={styles.select} 
-            open={open}
-              value={value}
-              items={items}
-              setOpen={setOpen}
-              setValue={setValue}
-              setItems={setItems}
+            open={aberto}
+            value={valor}
+            items={item}
+            setOpen={setAberto}
+            setValue={setValor}
+              setItems={setItem}
               placeholder="Municipios"
-            />
-            </View>
+              />
+              </View>
              <View>
             <Text style={styles.acessoText}>ACESSO</Text>
             </View>
             <View style={{ alignItems: 'center' }}>
+            </View>
+            <View>
             <DropDownPicker
             style={styles.acesso} 
-            open={open}
-            value={value}
-            items={items}
-            setOpen={setOpen}
-            setValue={setValue}
-            setItems={setItems}
-              placeholder="Acesso"
-            />
-            </View>
+            open={abertoAcesso}
+            value={valorAcesso}
+            items={itemAcesso}
+            setOpen={setAbertoAcesso}
+            setValue={setValorAcesso}
+              setItems={setItemAcesso}
+              placeholder="acesso"
+              />
+              </View>
             <View>
             <Text style={styles.localizacao}>LOCALIZAÇÃO</Text>
             </View>
@@ -137,28 +195,25 @@ async function requerente (dados) {
             />
             <Text>formulario step content</Text>
             </View>
-          </View>
+     
           </View>
 
           <View style={styles.form_step1}>
             <View style={styles.rect2}>
               <Text style={styles.titulo} >SETOR DE ABRANGÊNCIA</Text>
-              <View>
-            <Text style={styles.municipio}>Setor de Abrangência</Text>
             </View>
-            <View style={{ alignItems: 'center' }}>
+            <View>
             <DropDownPicker
-            style={styles.select} 
-            open={open}
-              value={value}
-              items={items}
-              setOpen={setOpen}
-              setValue={setValue}
-              setItems={setItems}
-              placeholder="Selecione"
-            />
-            </View>
-            </View>
+            style={styles.abrangencia} 
+            open={abertoAbrangencia}
+            value={valorAbrangencia}
+            items={itemAbrangencia}
+            setOpen={setAbertoAbrangencia}
+            setValue={setValorAbrangencia}
+              setItems={setItemAbrangencia}
+              placeholder="Selecione::"
+              />
+              </View>
             </View>
           </ProgressStep>
           <ProgressStep
@@ -313,23 +368,187 @@ async function requerente (dados) {
             </View>
           </ProgressStep>
           <ProgressStep
-            label="formulario3"
+            label="Atividades"
             onNext={onNextStep}
             onPrevious={onPrevStep}
-            scrollViewProps={defaultScrollViewProps}
+            scrollable={true}
           >
-            <View style={{ alignItems: 'center' }}>
-              <Text>formulario3 step content</Text>
+            <View style={styles.form3}>
+            <View style={styles.rect2}>
+            <Text style={styles.titulo} >INÍCIO DAS ATIVIDADES</Text>
+              </View>
+              <View style={styles.atividadeTitle}>
+              <Text >Inicio da Atividade</Text>
+              </View>
+            <View>
+            <DropDownPicker
+            style={styles.atividade} 
+            open={openDescricao}
+              value={valorAtividade}
+              items={itemDescricao}
+              setOpen={setOpenDescricao}
+              setValue={setValorAtividade}
+              setItems={setItemDescricao}
+              placeholder="Selecione::"
+            />
             </View>
+            </View>
+
+            <View style={styles.form4}>
+            <View style={styles.rect2}>
+            <Text style={styles.titulo} >NATUREZA E RAMO DA ATIVIDADE ECONÔMICA</Text>
+              </View>
+              <View style={styles.municipio}>
+              <Text >Natureza da Atividade</Text>
+              </View>
+            <View>
+            <DropDownPicker
+            style={styles.NaturezaAtv} 
+            open={openNaturezaAtv}
+              value={valorNaturezaAtv}
+              items={itemNaturezaAtv}
+              setOpen={setOpenNaturezaAtv}
+              setValue={setValorNaturezaAtv}
+              setItems={setItemNaturezaAtv}
+              placeholder="Selecione::"
+            />
+              </View>
+            
+            <View style={{ alignItems: 'center' }}> 
+            <TextInput 
+            style={styles.inputOutros}
+            onChangeText={setOutros}
+            value={outros}
+            placeholder={" Outros"}
+            />
+            </View>
+            <View>
+            <Text style={styles.NaturezaAtvTitle}>Ramo da Atividade</Text>
+            </View>
+            <View style={{ alignItems: 'center' }}> 
+            <TextInput 
+            style={styles.inputOutros}
+            onChangeText={setComercio}
+            value={comercio}
+            placeholder={"  Comércio"}
+            />
+            <TextInput 
+            style={styles.inputOutros}
+            onChangeText={setIndustria}
+            value={industria}
+            placeholder={"  Indústria"}
+            />
+            <TextInput 
+            style={styles.inputOutros}
+            onChangeText={setRecursosNaturais}
+            value={recursosNaturais}
+            placeholder={"  Recursos Naturais"}
+            />
+            <TextInput 
+            style={styles.inputOutros}
+            onChangeText={setT_I}
+            value={t_I}
+            placeholder={"   Tecnologia da Informação/Comunicação"}
+            />
+
+            </View>
+            </View>
+              
           </ProgressStep>
           <ProgressStep
-            label="formulario4"
+            label="Empregados/Associados"
+            onPrevious={onPrevStep}
+            onSubmit={onSubmitSteps}
+            scrollable={true}
+          >
+            <View style={styles.form5}>
+            <View style={styles.rect2}>
+            <Text style={styles.titulo} >NÚMERO DE EMPREGADOS E/OU ASSOCIADOS, COOPERADOS</Text>
+              </View>
+              <View style={styles.municipio}>
+              <Text >Mão de Obra Empregada</Text>
+              </View>
+              <View>
+            <DropDownPicker
+            style={styles.Mao_de_obra} 
+            open={openMao_de_obra}
+              value={valorMao_de_obra}
+              items={itemMao_de_obra}
+              setOpen={setOpenMao_de_obra}
+              setValue={setValorMao_de_obra}
+              setItems={setItemMao_de_obra}
+              placeholder="Selecione::"
+            />
+            </View>
+            <View style={styles.municipio}>
+              <Text >Número de Associados</Text>
+              </View>
+              <View>
+              <DropDownPicker
+            style={styles.Mao_de_obra} 
+            open={openSe_ruj_associados}
+              value={valorSe_ruj_associados}
+              items={itemSe_ruj_associados}
+              setOpen={setOpenSe_ruj_associados}
+              setValue={setValorSe_ruj_associados}
+              setItems={setItemSe_ruj_associados}
+              placeholder="Selecione::"
+            />
+              </View>
+
+              <View style={styles.municipio}>
+              <Text >Número de Cooperados</Text>
+              </View>
+              <View>
+              <DropDownPicker
+            style={styles.Mao_de_obra} 
+            open={openSe_ruj_cooperados}
+              value={valorSe_ruj_cooperados}
+              items={itemSe_ruj_cooperados}
+              setOpen={setOpenSe_ruj_cooperados}
+              setValue={setValorSe_ruj_cooperados}
+              setItems={setItemSe_ruj_cooperados}
+              placeholder="Selecione::"
+            />
+              </View>
+              </View>
+
+            <View style={styles.form6}>
+            <View style={styles.rect2}>
+            <Text style={styles.titulo} >POLÍTICA DE BENFÍCIOS</Text>
+              </View>
+              <View style={styles.municipio}>
+              <Text >Natureza da Atividade</Text>
+              </View>
+              <View>
+              <DropDownPicker
+            style={styles.Mao_de_obra} 
+            open={openSe_ruj_beneficios_concedidos}
+              value={valorSe_ruj_beneficios_concedidos}
+              items={itemSe_ruj_cooperados}
+              setOpen={setOpenSe_ruj_cooperados}
+              setValue={setValorSe_ruj_cooperados}
+              setItems={setItemSe_ruj_cooperados}
+              placeholder="Selecione::"
+            />
+              </View>
+              </View>
+
+            <View style={{ alignItems: 'center' }}>
+          
+              <Text>formulario4sdasd step content</Text>
+            </View>
+          </ProgressStep>
+
+          <ProgressStep
+            label="form5"
             onPrevious={onPrevStep}
             onSubmit={onSubmitSteps}
             scrollViewProps={defaultScrollViewProps}
           >
             <View style={{ alignItems: 'center' }}>
-              <Text>formulario4 step content</Text>
+          
+              <Text>form 5 step content</Text>
             </View>
           </ProgressStep>
         </ProgressSteps>
@@ -402,6 +621,41 @@ const styles = StyleSheet.create({
     borderColor: "rgba(74,144,226,1)",
     borderRadius: 3
   },
+  form3: {
+    width: 340,
+    height: 150,
+    marginLeft:25,
+    borderWidth: 1,
+    borderColor: "rgba(74,144,226,1)",
+    borderRadius: 3
+  },
+  form4: {
+    width: 340,
+    height: 450,
+    marginLeft:25,
+    marginTop:10,
+    borderWidth: 1,
+    borderColor: "rgba(74,144,226,1)",
+    borderRadius: 3
+  },
+  form5: {
+    width: 340,
+    height: 340,
+    marginLeft:25,
+    marginTop:10,
+    borderWidth: 1,
+    borderColor: "rgba(74,144,226,1)",
+    borderRadius: 3
+  },
+  form6: {
+    width: 340,
+    height: 300,
+    marginLeft:25,
+    marginTop:10,
+    borderWidth: 1,
+    borderColor: "rgba(74,144,226,1)",
+    borderRadius: 3
+  },
   form_step1: {
     marginTop:15,
     width: 340,
@@ -415,8 +669,33 @@ const styles = StyleSheet.create({
     height: 40,
     width:'85%',
     marginLeft:30,
-    
     height: 40,
+    zIndex:9999,
+    borderRadius:0,
+    borderWidth: 1,
+  },
+  atividade:{
+    marginTop:15,
+    zIndex:1000,
+    height: 40,
+    width:'85%',
+    marginLeft:30,
+    borderRadius:0,
+    borderWidth: 1,
+  },
+  NaturezaAtv:{
+    marginTop:5,
+    height: 40,
+    width:'85%',
+    marginLeft:30,
+    borderRadius:0,
+    borderWidth: 1,
+  },
+  Mao_de_obra:{
+    marginTop:5,
+    height: 40,
+    width:'85%',
+    marginLeft:30,
     borderRadius:0,
     borderWidth: 1,
   },
@@ -426,6 +705,15 @@ const styles = StyleSheet.create({
     marginLeft:30,
     height: 40,
     marginTop:5,
+    borderRadius:0,
+    borderWidth: 1,
+  },
+  abrangencia:{
+    height: 40,
+    width:'85%',
+    marginLeft:30,
+    height: 40,
+    marginTop:20,
     borderRadius:0,
     borderWidth: 1,
   },
@@ -439,6 +727,14 @@ const styles = StyleSheet.create({
     height: 40,
     width:'85%',
     marginTop:2,
+    borderWidth: 1,
+    backgroundColor:'white'
+  },
+  inputOutros: {
+    height: 40,
+    width:'85%',
+    marginTop:10,
+    marginLeft:10,
     borderWidth: 1,
     backgroundColor:'white'
   },
@@ -459,14 +755,13 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(74,144,226,1)",
     borderRadius: 3
   },
+  
   loremIpsum: {
-
     color: "rgba(255,255,255,1)",
     marginTop: 11,
     marginLeft: 13
   },
   cnpj: {
-
     color: "#121212",
     marginTop: 15,
     marginLeft: 9
@@ -474,8 +769,18 @@ const styles = StyleSheet.create({
   municipio: {
     color: "#121212",
     marginLeft:30,
-    marginTop:25,
+    marginTop:15,
     marginBottom:9
+  },
+  atividadeTitle: {
+    color: "#121212",
+    marginLeft:30,
+    marginTop:10,
+  },
+  NaturezaAtvTitle: {
+    color: "#121212",
+    marginLeft:30,
+    marginTop:10,
   },
   acessoText: {
     color: "#121212",
@@ -502,6 +807,6 @@ const styles = StyleSheet.create({
   titulo: {
     color: "white",
     marginLeft: 9,
-    marginTop:5
+    marginTop:1
   },
 });
