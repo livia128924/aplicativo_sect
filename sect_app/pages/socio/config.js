c266ea1ecb92cb1c29928ac06c7bb1f1c33f8ec0
livia128924 <livia128924@gmail.com>
2021-07-06 12:50:17 -0300

    config

--- sect_app/pages/socio/config.js
+++ sect_app/pages/socio/config.js
@@ -1,13 +1,51 @@
 import React, { Component, useEffect } from 'react';
 import axios from 'axios';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { DatabaseConnection } from '../database/database';
 import Mybutton from '../components/Mybutton';
 const db = DatabaseConnection.getConnection();
 
 function Config({ navigation }) {
 
+  async function load_dados() {
+
+    axios.post('http://192.168.0.151:8082/api/metas.php', {})
+      .then(function (response) {
+        const dados_metas = response.data;
+        //console.log(dados2);
+        var arr = [];
+        Object.keys(dados_metas).forEach(function (key, i) {
+          arr.push(key);
+        });
+        var arr_colunas = [];
+        var novo_array = [];
+        arr.forEach((item) => {
+          arr_colunas.push(Object.keys(dados_metas[item]));
+          novo_array[item] = Object.keys(dados_metas[item]);
+          console.log(dados_metas[item]);
+          // db.transaction((tx) => {
+          //   tx.executeSql(dados_metas[item], []);
+
+
+          // }, (err) => {
+          //   console.error("There was a problem with the tx metas", err);
+          //   return true;
+          // }, (success) => {
+          //   console.log("dados_metas", success);
+          // });
+
+
+        });
+      })
+      .catch(function (error) {
+        console.log('Erro');
+      });
+
+
+  }
+
   useEffect(() => {
+
     //cria as tabelas
     axios.post('http://192.168.0.151:8082/api/estrutura2.php', {})
       .then(function (response) {
@@ -61,48 +99,46 @@ function Config({ navigation }) {
         console.log('Erro');
       });
 
-      //function dados ()
+    //function dados ()
 
-     axios.post('http://192.168.0.151:8082/api/dados2.php', {})
-     .then(function (response) {
-       const  dados2 = response.data;
-       //console.log(dados2);
-       var arr = [];
-       Object.keys(dados2).forEach(function (key, i) {
-         arr.push(key);
-       });
-       var arr_colunas = [];
-       var novo_array = [];
-       arr.forEach((item) => {
-         arr_colunas.push(Object.keys(dados2[item]));
-         novo_array[item] = Object.keys(dados2[item]);
-         //console.log(dados2[item]);
-         db.transaction((tx) => {
-           tx.executeSql(dados2[item],[]);
-           tx.executeSql(
-             "select descricao from aux_acesso",[],  (tx, results) =>{
-              var len = results.rows.length, i;
-              console.log(len);
-              for(i = 0; i< len; i++){
-                console.log(results.rows.item(i));
+    axios.post('http://192.168.0.151:8082/api/dados2.php', {})
+      .then(function (response) {
+        const dados2 = response.data;
+        //console.log(dados2);
+        var arr = [];
+        Object.keys(dados2).forEach(function (key, i) {
+          arr.push(key);
+        });
+        var arr_colunas = [];
+        var novo_array = [];
+        arr.forEach((item) => {
+          arr_colunas.push(Object.keys(dados2[item]));
+          novo_array[item] = Object.keys(dados2[item]);
+          //console.log(dados2[item]);
+          db.transaction((tx) => {
+            tx.executeSql(dados2[item], []);
+            tx.executeSql(
+              "select descricao from aux_acesso", [], (tx, results) => {
+                var len = results.rows.length, i;
+                console.log(len);
+                for (i = 0; i < len; i++) {
+                  console.log(results.rows.item(i));
+                }
               }
-             }
-           );
+            );
 
-         },(err) => {
-           console.error("There was a problem with the tx", err);
-           return true;
-         },(success ) => {
-           console.log("all done",success );
-         });
+          }, (err) => {
+            console.error("There was a problem with the tx dados2", err);
+            return true;
+          }, (success) => {
+            console.log("all done = select ", success);
+          });
 
-
-       });
-     })
-     .catch(function (error) {
-       console.log('Erro');
+        });
+      })
+      .catch(function (error) {
+        console.log('Erro');
       });
-
 
   }, []);
 
@@ -111,16 +147,21 @@ function Config({ navigation }) {
     <View>
       <Text>Teste
       </Text>
+
       <Mybutton
-      title='Estrutura tb aux'
+        title='drop and creat tabelas pri'
+        customClick={load_dados}
+      />
+
+      <Mybutton
+        title='dados tb aux'
       //customClick={() => navigation.navigate('Config')}
-  />
-       <Mybutton
-      title='dados tb aux'
-      //customClick={() => navigation.navigate('Config')}
-  />
+      />
     </View>
   )
 
 }
 export default Config;
+
+
+
